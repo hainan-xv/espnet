e2321b9dace17b52bdb6f9dd166da3988c63d69d
Hainan Xu <hainan.xv@gmail.com>
2018-06-06 19:39:25 -0400

    add tri-letter for training, change scoring

--- src/utils/score_sclite.sh
+++ src/utils/score_sclite.sh
@@ -41,6 +41,13 @@ if [ ! -z ${filter} ]; then
     sed -i.bak3 -f ${filter} ${dir}/hyp.trn
     sed -i.bak3 -f ${filter} ${dir}/ref.trn
 fi
+
+for i in hyp ref; do
+  mv $dir/$i.trn $dir/$i.trn.tri
+  cat $dir/$i.trn.tri | awk '{print $NF}' > $dir/tmp.uttid.$i
+  cat $dir/$i.trn.tri | awk '{for(i=1;i<NF;i++) printf("%s ",$i); print""}' | sed "s=-= =g" | awk '{for(i=2;i<=NF;i+=3) printf("%s ",$i); print""}' > $dir/tmp.content.$i
+  paste $dir/tmp.content.$i $dir/tmp.uttid.$i > $dir/$i.trn
+done
     
 sclite -r ${dir}/ref.trn trn -h ${dir}/hyp.trn trn -i rm -o all stdout > ${dir}/result.txt
 
